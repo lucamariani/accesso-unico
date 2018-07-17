49610b4bda7f5a03e0e6a54ff65f73c2c4fa4fb5
Luca Mariani <luca.marianj@gmail.com>
2018-07-17 16:05:55 +0200

    restyled home search and added search script for it

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,4 @@
+/** Header search functions **/
 $(function() {
   var $projects = $('#projects > ul > li').clone()
 
@@ -26,5 +27,37 @@ $(function() {
       }
     }))
     $('#projects > ul > li:first').trigger('mouseenter')
+  })
+})
+
+/** Home search functions **/
+$(function() {
+  var $projects = $('#home-projects > ul > li').clone()
+
+  $('#home-search-form').on('submit', function() {
+    var $sel = $('#home-projects > ul > li.is-selected')
+    if ($sel.length > 0) {
+      window.location = $sel.find('a').attr('href')
+      return false
+    }
+  })
+
+  $('#home-cerca').on('keydown', function(event) {
+    if (event.which === 9 && $(this).is(':focus')) {
+      $(this).blur()
+      return true
+    }
+  })
+
+  $('#home-cerca').on('keyup', function(event) {
+    var text = event.target.value.toLowerCase()
+    $('#home-projects > ul').html($projects.filter(function(index, elem) {
+      var project = $(elem).find('span').html().toLowerCase()
+
+      if (project.indexOf(text) >= 0) {
+        return elem
+      }
+    }))
+    $('#home-projects > ul > li:first').trigger('mouseenter')
   })
 })
