3ac6539d3941d442dac82d7d97fa5ed4e0a3c901
Luca Mariani <luca.marianj@gmail.com>
2018-09-23 19:35:06 +0200

    added docuemnts' search result style

--- assets/scripts/search_docs.js
+++ assets/scripts/search_docs.js
@@ -35,19 +35,24 @@ function displaySearchResults(results, store) {
       console.log('key:' + key);
       var item = store[key];
       console.log('item:' + item);
-      appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
+
+      appendString += '<div class="Grid-cell u-md-size1of3 u-lg-size1of3 u-margin-r-bottom u-layout-matchHeight">';
+      appendString += '<div class="u-nbfc u-flexWrap u-flex u-color-grey-60 u-xs-padding-all-none u-borderShadow-m u-xs-borderShadow-none u-borderRadius-m u-background-white u-sizeFill">';
+      appendString += '<div class="u-flexWrap u-flex u-flexAlignSelfStretch u-sizeFill u-padding-r-all"><div class="u-sizeFull u-padding-r-all u-xs-padding-all-none">';
+      appendString += '<a href="' + item.url + '" target="_blank"><h3 class="u-color-50">' + item.title + '</h3></a>';
       appendString += '<p>Categoria: ' + item.category + '</p>';
       appendString += '<p>Tema: ' + item.tema + '</p></li>';
       appendString += '<p>Numero: ' + item.number + '</p>';
-      appendString += '<p>Anno: ' + item.year + '</p></li>';
+      appendString += '<p>Anno: ' + item.year + '</p>';
+      appendString += '</div></div></div></div>';
     }
 
     searchResults.html(appendString);
   } else {
-    searchResults.html('<li>No results found</li>');
+    searchResults.html('<p>La ricerca non ha prodotto alcun risultato</p>');
   }
   // show results' section
-  searchResults.show();
+  $('#result-section').show();
 }
 
 /** Docs search functions **/
@@ -105,6 +110,8 @@ $(function() {
     var title = $('#titolo').val();
     var number = $('#numero').val();
     var year = $('#year option:selected').val();
+    var theme = $('#tema option:selected').val();
+    var category = $('#categoria option:selected').val();
     console.log('searching for titolo: ' + title + ', number: ' + number + ',year: ' + year);
 
     var search_title = '*' + title + '*';
@@ -113,6 +120,10 @@ $(function() {
     var search_pattern = '+title:' + search_title + ' +number:' + search_number;
     if ( search_year.indexOf('---') < 0 )
       search_pattern += ' +year:' + search_year;
+    if ( theme.indexOf('---') < 0 )
+      search_pattern += ' +tema:' + theme;
+    if ( category.indexOf('---') < 0 )
+      search_pattern += ' +category:' + category;
 
     console.log('searching for ' + search_pattern);
     var results = idx.search(search_pattern);
