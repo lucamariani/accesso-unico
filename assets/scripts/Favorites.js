f3e9956ebe9ccb150c955f34a17a5f33832fc65b
Luca Mariani <luca.marianj@gmail.com>
2018-11-04 02:32:54 +0100

    favorite script working

--- assets/scripts/Favorites.js
+++ assets/scripts/Favorites.js
@@ -1,4 +1,18 @@
+/**
+ * @author Luca Mariani
+ * @since 03/11/2018
+ */
+
 window.Favorites = window.Favorites || {};
+
+/**
+ * Favorites
+ *
+ * This module is used to simulate user's favorites saving.
+ * It uses a cookie to save them.
+ *
+ * @module Favorites
+ */
 
 (function (exports) {
 
@@ -21,10 +35,13 @@ window.Favorites = window.Favorites || {};
     Cookies.set(FAVORITES_COOKIE,Favorites.getAll())
   });
 
-  //test
-  exports.debug = function() {
-    console.log(favorites)
-  }
+  /**
+   * Bind event handlers on DOM ready
+   */
+  $(function() {
+    _toogleFavoriteButton()
+    _bindEventHandlers()
+  });
 
   /**
    * Add a favorite
@@ -33,6 +50,7 @@ window.Favorites = window.Favorites || {};
    * @param {String} url the favorite's url
    */
   exports.add = function(name, url) {
+    console.log('adding ' + name + ':' + url);
     favorites[name] = url
   }
 
@@ -42,6 +60,7 @@ window.Favorites = window.Favorites || {};
    * @param {String} name the favorite's name to be removed
    */
   exports.remove = function(name) {
+    console.log('removing ' + name);
     delete favorites[name]
   }
 
@@ -64,4 +83,51 @@ window.Favorites = window.Favorites || {};
   exports.reset = function() {
     favorites = {}
   }
+
+  /**
+   * Change favorite handler button
+   *
+   * Check in favorites to see if this scheda is already stored
+   * and hide/show buttons accordingly
+   */
+  function _toogleFavoriteButton() {
+    const title = $('#handle-favorite').attr('scheda_title')
+    if ( favorites.hasOwnProperty(title) ) {
+      $('#add-scheda-to-favorites-btn').hide()
+      $('#remove-scheda-from-favorites-btn').show()
+    } else {
+      $('#add-scheda-to-favorites-btn').show()
+      $('#remove-scheda-from-favorites-btn').hide()
+    }
+  }
+
+  /**
+   * Bind event handlers
+   */
+   function _bindEventHandlers() {
+     /**
+      * Event: add scheda to favorite button "clicked"
+      *
+      * Add the scheda in user's favorites
+      */
+      $('#add-scheda-to-favorites-btn').click(function() {
+        var title = $(this).parent().attr('scheda_title')
+        var url = $(this).parent().attr('scheda_url')
+        Favorites.add(title, url)
+        //change button
+        _toogleFavoriteButton()
+      })
+
+      /**
+       * Event: remove scheda from favorite button "clicked"
+       *
+       * Remove the scheda from user's favorites
+       */
+       $('#remove-scheda-from-favorites-btn').click(function() {
+         const title = $(this).parent().attr('scheda_title')
+         Favorites.remove(title)
+         //change button
+         _toogleFavoriteButton()
+       })
+   }
 })(Favorites)
