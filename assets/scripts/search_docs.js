410717c3e333c3d0bc265322f6614f10d94861cd
Luca Mariani <luca.marianj@gmail.com>
2018-11-19 10:23:04 +0100

    wip: minor fixes

--- assets/scripts/search_docs.js
+++ assets/scripts/search_docs.js
@@ -45,8 +45,8 @@ function displaySearchResults(results, store) {
 
       appendString += '<div class="Grid-cell u-md-size1of3 u-lg-size1of3 u-margin-r-bottom u-layout-matchHeight">';
       appendString += '<div class="u-nbfc u-flexWrap u-flex u-color-grey-60 u-xs-padding-all-none u-borderShadow-m u-xs-borderShadow-none u-borderRadius-m u-background-white u-sizeFill">';
-      appendString += '<div class="u-flexWrap u-flex u-flexAlignSelfStretch u-sizeFill u-padding-r-all"><div class="u-sizeFull u-padding-r-all u-xs-padding-all-none single-doc">';
-      appendString += '<a href="' + item.url + '"><h3 class="u-color-50">' + item.title + '</h3></a>';
+      appendString += '<div class="docs-box u-flexWrap u-flex u-flexAlignSelfStretch u-sizeFill u-padding-r-all"><div class="u-sizeFull u-padding-r-all u-xs-padding-all-none single-doc">';
+      appendString += '<a target="_blank" href="' + item.url + '"><h3 class="u-color-50">' + item.title + '</h3></a>';
       appendString += '<p class="object"><b>' + item.subject + '</b></p>';
 
       if ( item.category.length > 0 )
@@ -79,6 +79,9 @@ function displaySearchResults(results, store) {
         docTags += '</p>';
         appendString += docTags;
       }
+
+      appendString += '<a href="' + item.url + '">' +
+                        '<button class="Button u-background-white u-color-50 u-sizeFull go-servizio-btn">Vai al documento</button></a>';
 
       appendString += '</div></div></div></div>';
     }
